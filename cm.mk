3246df87434315a161d40b8c0afd6b60e669f103
Ivan Cristina <ivancristina5@gmail.com>
2017-01-03 01:32:21 +0100

    Config by pchatzop

--- cm.mk
+++ cm.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, device/samsung/jactivelte/full_jactivelte.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
